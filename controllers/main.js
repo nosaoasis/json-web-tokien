10fed2cfe9aee0fb9eafe4ceb3418250d7514779
Nosa Agho <nosaoasis@gmail.com>
2022-08-05 16:56:06 +0100

    JWT Authorizationa and Middleware
    Implemented the jwt features and also created a middleware function to perfomr the authentication feature for routes that need authentication

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,33 @@
+require("dotenv").config();
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
-  res.send("fake login.register")
-}
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError(
+      "please provide username and password....thanx",
+      400
+    );
+  }
+
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+  res.status(200).json({ msg: `user created`, token });
+};
 const dashboard = async (req, res) => {
-  const luckyNum = Math.floor(Math.random() * 100)
-  res.status(200).json({msg:`hello john doe`, secret:`your secret number is ${luckyNum}`})
-}
+  const {id, username} = req.user
+  const luckyNum = Math.floor(Math.random() * 100);
+
+  res.status(200).json({
+    msg: `hello ${username}`,
+    secret: `your id is ${id} and your secret number is ${luckyNum}`,
+  });
+};
 
 module.exports = {
-  login, 
-  dashboard
-}
+  login,
+  dashboard,
+};
